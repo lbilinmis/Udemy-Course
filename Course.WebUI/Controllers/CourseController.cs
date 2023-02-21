30b2ce4f76206c40fd3c1bf839ffca7782d577fc
lbilinmis <lbilinmis@gelisim.edu.tr>
2023-02-21 20:28:10 +0300

    Core ilk proje update controller

--- Course.WebUI/Controllers/CourseController.cs
+++ Course.WebUI/Controllers/CourseController.cs
@@ -33,7 +33,7 @@ namespace Course.WebUI.Controllers
         {
             if (ModelState.IsValid)
             {
-                Repository.RepositoryData.AddStudent(student);
+                RepositoryData.AddStudent(student);
                 return View("Thanks", student);
 
             }
