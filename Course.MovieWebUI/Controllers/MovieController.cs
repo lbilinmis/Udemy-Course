9f6159bd5afebb32779b9f2b0ec6bf91e94adb15
lbilinmis <lbilinmis@gelisim.edu.tr>
2023-02-22 19:23:31 +0300

    New Project Movie App Web UI

---
+++ Course.MovieWebUI/Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Course.MovieWebUI.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+    }
+}
